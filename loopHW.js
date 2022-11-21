1412a10e7215b04a257c1245a3736dae04191f3d
Albruce Appesh <albruceappesh@gmail.com>
2022-11-21 02:17:15 -0500

    first commit

---
+++ loopHW.js
@@ -0,0 +1,19 @@
+// write a for loop that logs 0-15
+
+for (i=0; i<=15; i++){
+    console.log(i);
+}
+
+// write a for loop that counts from 15-0
+
+for(i=15; i>=0; i--){
+    console.log(i);
+}
+
+// make variable laps=0, write a for loop that logs ("that's another lap!") and adds 1 to the laps variable.
+
+let laps=0;
+for(i=1; i<=8; i++){
+    console.log("That's another lap!");
+    laps++;
+}console.log(laps)
